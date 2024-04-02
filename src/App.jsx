a6da4bf79021a7189b43698966b4f2ca9f134df3
Vivian Davila <viviananddav@gmail.com>
2024-04-01 23:35:38 -0700

    Edits

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,25 @@ let accessToken = import.meta.env.VITE_LOCATION_ACCESS_TOKEN; //get from .env fi
 console.log("Access Token", accessToken);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState('');
+  const [location, setLocation] = useState({});
 
   function handleNewCity(e) {
+    setCity(e.target.value);
+  }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    getLocation(city);
+  }
+  function getLocation() {
+    console.log("Getting Location Information for", city);
   }
 
   return (
     <>
-      <form>
-        <input placeholder="Type a City Name" />
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Type a City Name" onChange={handleNewCity} />
       </form>
     </>
   )
